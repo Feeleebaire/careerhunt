fa106092c83fc9bbd5bbc8aee66422d63a5ae42a
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-06-23 11:14:05 +0200

    api okay

--- Gemfile
+++ Gemfile
@@ -16,6 +16,8 @@ gem 'jquery-rails'
 gem 'sass-rails'
 gem 'simple_form'
 gem 'uglifier'
+gem 'rest-client'
+
 
 group :development, :test do
   gem 'better_errors'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,30 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
+  require "json"
+  require "rest-client"
+
+
 
   def home
+
+response = RestClient.post( "https://api.producthunt.com/v1/oauth/token",
+  {
+    :client_id => ENV['clientid'],
+    :client_secret => ENV['clientsecret'],
+    :grant_type => "client_credentials"
+    }
+  )
+  acces_token = JSON.parse(response)
+  your_access_token = acces_token['access_token']
+
+
+  lolo= RestClient.get 'https://api.producthunt.com/v1/posts', {:Authorization => "Bearer #{your_access_token}"}
+  zbra = JSON.parse(lolo)
+
+
+   raise
+
   end
 end
+
+
