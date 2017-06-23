b694c984e78f957c9c839570b3df2676b4446d19
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-06-23 11:18:52 +0200

    refactro

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,22 +7,19 @@ class PagesController < ApplicationController
 
   def home
 
-response = RestClient.post( "https://api.producthunt.com/v1/oauth/token",
-  {
-    :client_id => ENV['clientid'],
-    :client_secret => ENV['clientsecret'],
-    :grant_type => "client_credentials"
-    }
-  )
-  acces_token = JSON.parse(response)
-  your_access_token = acces_token['access_token']
+  response = RestClient.post( "https://api.producthunt.com/v1/oauth/token",
+    {
+      :client_id => ENV['clientid'],
+      :client_secret => ENV['clientsecret'],
+      :grant_type => "client_credentials"
+      }
+    )
+    acces_token = JSON.parse(response)
+    your_access_token = acces_token['access_token']
 
 
-  lolo= RestClient.get 'https://api.producthunt.com/v1/posts', {:Authorization => "Bearer #{your_access_token}"}
-  zbra = JSON.parse(lolo)
-
-
-   raise
+    lolo= RestClient.get 'https://api.producthunt.com/v1/posts', {:Authorization => "Bearer #{your_access_token}"}
+    zbra = JSON.parse(lolo)
 
   end
 end
