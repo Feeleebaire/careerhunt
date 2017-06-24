80fc66ecb553caabdd96118a6522b23ca190c30d
Philibert Latournerie <philibert.latournerie@hotmail.fr>
2017-06-24 14:18:13 +0200

    okay api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,7 @@ class PagesController < ApplicationController
 
 
   def home
-
+  #demande d'accès à l'API
   response = RestClient.post( "https://api.producthunt.com/v1/oauth/token",
     {
       :client_id => ENV['clientid'],
@@ -14,12 +14,13 @@ class PagesController < ApplicationController
       :grant_type => "client_credentials"
       }
     )
-    acces_token = JSON.parse(response)
-    your_access_token = acces_token['access_token']
+  acces_token = JSON.parse(response)
+  #Récupération de l'accès token
+  your_access_token = acces_token['access_token']
 
-
-    query = RestClient.get 'https://api.producthunt.com/v1/categories', {:Authorization => "Bearer #{your_access_token}"}
-    @users = JSON.parse(query)
+  #Requête pour récupérer les catégories
+  query = RestClient.get 'https://api.producthunt.com/v1/categories', {:Authorization => "Bearer #{your_access_token}"}
+  @users = JSON.parse(query)
 
   end
 end
